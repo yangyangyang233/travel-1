2bfe9d79902dc57d4a84e774195cb2586749d878
Tyler <Tyleryq@Gmail.com>
2020-07-20 17:22:20 +0800

    Signed-off-by: Tyler <Tyleryq@Gmail.com>

--- src/main/java/cn/itcast/travel/util/MailUtils.java
+++ src/main/java/cn/itcast/travel/util/MailUtils.java
@@ -9,8 +9,8 @@ import java.util.Properties;
  * 发邮件工具类
  */
 public final class MailUtils {
-    private static final String USER = "2078625535@qq.com"; // 发件人称号，同邮箱地址
-    private static final String PASSWORD = "nzxrbalxvmdmehjf"; // 如果是qq邮箱可以使户端授权码，或者登录密码
+    private static final String USER = "2127269781@qq.com"; // 发件人称号，同邮箱地址
+    private static final String PASSWORD = "ijdjokspcbnzfbfa"; // 如果是qq邮箱可以使户端授权码，或者登录密码
 
     /**
      *
